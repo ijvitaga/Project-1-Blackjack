f54a3b96bf35ca5e521e6dba2af2579cb456d031
Ian James Vitaga <ianjamesvitaga@Loris-MacBook-Air.local>
2020-05-08 15:02:14 -0700

    Added my Project

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/*------ constants ------*/
+// Have a constant called "players" and
+    // Have a dealer with the value of 1
+    // Have a player1 with a value of -1
+
+    // Have an array of winning combos
+
+    // Have an array of winning black jack combos
+
+
+
+
+
+/*------ app's state (variables) ------*/
+    // Here is where the hit and stay buttons will go
+
+    // Here is where the turn will be let for player or dealer 
+
+    // Here is where the winner outcome will be let to 1 = dealer, -1 = player1, 'B' = bust, 'BJ1' = dealer with blackjack, 'BJ-1' = player with blackjack, 'P' = tie
+
+
+
+
+
+/*------ cached element references ------*/
+
+
+
+
+
+/*------ event listners ------*/
+    // Here is where the listen for click on the hit or stay buttons will go
+
+
+
+
+/*------ functions ------*/
+// function #1 for click event:for stay and hit buttons
+
+// function #2 for checkForWin: check for--> winning combos, bust combos(are the cards set up with values to where we can just check to see if the player is over 21?), blackjack combos, Push combos? (do we need this?)
+
+// function #3 to display/update scores
+
+// function #4 render msg: 
+// "You win!" for '-1' win, "Dealer wins" for '1' win, "Blackjack!" for '-1' with blackjack combos, "Ouch You lose" for '1' with blackjack combos, "Your turn" for '-1' turn, "Dealer's turn" for '1' turn
+
+// function #5 render: "players" scores
